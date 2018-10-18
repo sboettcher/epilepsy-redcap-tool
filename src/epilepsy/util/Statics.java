c77d1956ceaeb445300c8ca954aad1540c039548
Sebastian Boettcher <seb.boett@gmail.com>
2018-10-18 19:39:00 +0200

    buildinfo

--- src/epilepsy/util/Statics.java
+++ src/epilepsy/util/Statics.java
@@ -4,4 +4,6 @@ import java.util.logging.Level;
 
 public class Statics {
   public static final Level loglvl = Level.FINE;
+  public static final String buildtime = "20181018_192655";
+  public static final String buildversion = "eb0be72";
 }
